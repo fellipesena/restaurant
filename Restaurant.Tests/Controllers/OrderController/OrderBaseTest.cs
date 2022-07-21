03f13b33f750157a80fec0f537f514036b0119dc
Fellipe Sena <pfosena@gmail.com>
2022-07-20 22:12:48 -0300

    Add OrderController test

---
+++ Restaurant.Tests/Controllers/OrderController/OrderBaseTest.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using Moq;
+using Restaurant.API.Interfaces.Services;
+using Restaurant.API.Models;
+using System.Collections.Generic;
+
+namespace Restaurant.Tests.Controllers.OrderController
+{
+    public class OrderBaseTest
+    {
+        protected readonly Mock<IOrderService> _orderService;
+        protected readonly Mock<IWaiterService> _waiterService;
+        protected readonly Mock<IBillService> _billService;
+        protected readonly Faker _faker;
+        protected readonly List<OrderItems> _orderItems;
+
+        public OrderBaseTest()
+        {
+            _orderService = new Mock<IOrderService>();
+            _waiterService = new Mock<IWaiterService>();
+            _billService = new Mock<IBillService>();
+            _faker = new();
+            _orderItems = new Mock<List<OrderItems>>().Object;
+        }
+    }
+}
---
+++ Restaurant.Tests/Controllers/OrderController/PostOrderTest.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Restaurant.API.Controllers;
+using Restaurant.API.Models;
+using System;
+using Xunit;
+
+namespace Restaurant.Tests.Controllers.OrderController
+{
+    public class PostOrderTest : OrderBaseTest
+    {
+        [Fact]
+        public void ShouldReturnBadRequest_WhenWaiterDoestnExists()
+        {
+            _ = _waiterService.Setup(_ => _.Get(It.IsAny<Waiter>())).Returns((Waiter)null);
+
+            OrdersController ordersController = new(_orderService.Object, _waiterService.Object, _billService.Object);
+
+            int waiterId = _faker.Random.Int();
+            ActionResult result = ordersController.PostOrder(_orderItems, waiterId, _faker.Random.Int()).Result;
+
+            BadRequestObjectResult objectResult = Assert.IsType<BadRequestObjectResult>(result);
+            
+            Assert.Equal($"No one waiter with id {waiterId} was found", objectResult.Value);
+        }
+
+        [Fact]
+        public void ShouldReturnBadRequest_WhenTableDoesntHaveBillOpen()
+        {
+            _ = _waiterService.Setup(_ => _.Get(It.IsAny<Waiter>())).Returns(new Waiter());
+            _ = _billService.Setup(_ => _.GetByTableNumber(It.IsAny<Bill>())).Returns((Bill)null);
+
+            int tableNumber = _faker.Random.Int();
+            OrdersController ordersController = new(_orderService.Object, _waiterService.Object, _billService.Object);
+
+            ActionResult result = ordersController.PostOrder(_orderItems, _faker.Random.Int(), tableNumber).Result;
+
+            BadRequestObjectResult objectResult = Assert.IsType<BadRequestObjectResult>(result);
+
+            Assert.Equal($"No one openned bill to table {tableNumber} was found", objectResult.Value);
+        }
+
+        [Fact]
+        public void ShouldReturnBadRequest_WhenInsertOrderReturnException()
+        {
+            _ = _waiterService.Setup(_ => _.Get(It.IsAny<Waiter>())).Returns(new Waiter());
+            _ = _billService.Setup(_ => _.GetByTableNumber(It.IsAny<Bill>())).Returns(new Bill());
+            
+            string exceptionMessage = _faker.Random.Word();
+            _ = _orderService.Setup(_ => _.Insert(It.IsAny<Order>())).Throws(new Exception(exceptionMessage));
+
+            OrdersController ordersController = new(_orderService.Object, _waiterService.Object, _billService.Object);
+
+            ActionResult result = ordersController.PostOrder(_orderItems, _faker.Random.Int(), _faker.Random.Int()).Result;
+
+            BadRequestObjectResult objectResult = Assert.IsType<BadRequestObjectResult>(result);
+
+            Assert.Equal(exceptionMessage, objectResult.Value);
+        }
+
+        [Fact]
+        public void ShouldReturnOkAndOrder_WhenPostNewOrder()
+        {
+            _ = _waiterService.Setup(_ => _.Get(It.IsAny<Waiter>())).Returns(new Waiter());
+            _ = _billService.Setup(_ => _.GetByTableNumber(It.IsAny<Bill>())).Returns(new Bill());
+            _ = _orderService.Setup(_ => _.Insert(It.IsAny<Order>())).Returns(new Order());
+
+            OrdersController ordersController = new(_orderService.Object, _waiterService.Object, _billService.Object);
+
+            ActionResult result = ordersController.PostOrder(_orderItems, _faker.Random.Int(), _faker.Random.Int()).Result;
+
+            OkObjectResult objectResult = Assert.IsType<OkObjectResult>(result);
+
+            Assert.NotNull(objectResult.Value);
+        }
+    }
+}
